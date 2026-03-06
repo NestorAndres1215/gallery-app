cf86469b09504c4268b5830ec7f25a8aa4cac80a
NestorAndres1215 <atiroandres@gmail.com>
2026-03-05 19:42:20 -0500

    subiendo

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
   onSearch: (q: string) => void;
